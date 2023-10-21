9931d47e6d810fc65125cd1a094e45fbeeffc771
me_madness <p_rangel@abv.bg>
2023-10-21 19:39:12 +0200

    2

--- 01. Programming Fundamentals Final Exam Retake - 15.08.2020/01. the_imitation_game.py
+++ 01. Programming Fundamentals Final Exam Retake - 15.08.2020/01. the_imitation_game.py
@@ -1,12 +1,12 @@
+encrypted_message = input().split()
 
 while True:
-    encrypted_message = input().split()
     command = input().split("|")
     if command[0] == "Decode":
         break
     elif command[0] == "Move":
         index = command[1]
-        for letter in encrypted_message(int(index)):
+        for letter in encrypted_message:
             encrypted_message.remove(letter)
             encrypted_message.append(letter)
             print(encrypted_message)
@@ -14,6 +14,7 @@ while True:
         index_value = command[1]
         insert_value = command[2]
         encrypted_message.insert(int(index_value), insert_value)
+        # encrypted_message.insert(int(index_value), insert_value)
         # encrypted_message = list(map(lambda x: x.insert(index_value, insert_value), encrypted_message))
         print(encrypted_message)
     elif command[0] == "ChangeAll":
